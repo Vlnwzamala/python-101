d698120eb42c8c4e4842f9656e96a978a90c8545
Vlnwzamala <mrvee4560@gmail.com>
2026-07-01 11:09:18 +0700

    day 1

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 print("hello world")
 print("information network engineering")
 print("chanachai")
+print("i'm chanachai , i will be a good engineer")
+print('he ask me "who got the book"')
+print("i'm good and \"i will do my best\" ")
+print('/\\')
+print('\ta\na\ta\ta\n\ta')
+print('''\ta
+a\ta\ta
+\ta''')
